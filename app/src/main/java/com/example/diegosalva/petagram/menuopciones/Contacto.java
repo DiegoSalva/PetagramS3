5de6c98788f0d683302eef17be58df2738c29e68
DIEGO SALVA <diegosalvam@gmail.com>
2016-06-09 22:57:48 -0500

    VSem4

--- app/src/main/java/com/example/diegosalva/petagram/menuopciones/Contacto.java
+++ app/src/main/java/com/example/diegosalva/petagram/menuopciones/Contacto.java
@@ -2,8 +2,15 @@ package com.example.diegosalva.petagram.menuopciones;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.diegosalva.petagram.R;
+
+import javax.mail.internet.AddressException;
 
 public class Contacto extends AppCompatActivity {
 
@@ -11,5 +18,37 @@ public class Contacto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacto);
+
+        Toolbar miActionBar = (Toolbar) findViewById(R.id.miActionBar);
+        setSupportActionBar(miActionBar);
+
+        //quitar icono 5 estrellas
+        ImageView iv5Stars = (ImageView) findViewById(R.id.ivFiveStarts);
+        iv5Stars.setVisibility(View.INVISIBLE);
+
+    }
+
+    public void enviaEmail(View v){
+        EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        EditText etNombre = (EditText) findViewById(R.id.etNombre);
+        EditText etMensaje = (EditText) findViewById(R.id.etMensaje);
+
+        Toast.makeText(getBaseContext(), " Tu : " + etNombre.getText()  + " con mail : " + etEmail.getText()
+                        + " Enviaste el mensaje: " + etMensaje.getText()
+                , Toast.LENGTH_LONG).show();
+
+    }
+
+    public void enviarComentarios(View v) throws AddressException {
+
+        EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        EditText etNombre = (EditText) findViewById(R.id.etNombre);
+        EditText etMensaje = (EditText) findViewById(R.id.etMensaje);
+        String email;
+        String nombre;
+        String mensaje;
+        email = etEmail.getText().toString();
+        nombre = etNombre.getText().toString();
+        mensaje = etMensaje.getText().toString();
     }
 }
